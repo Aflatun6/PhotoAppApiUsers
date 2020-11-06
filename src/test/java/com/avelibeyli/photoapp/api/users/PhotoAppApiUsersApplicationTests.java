16e1fa2449a17e632429b27f620daf078142ec27
004462 <a.velibeyli6@gmail.com>
2020-11-06 16:45:44 +0400

    Initial commit

---
+++ src/test/java/com/avelibeyli/photoapp/api/users/PhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avelibeyli.photoapp.api.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
